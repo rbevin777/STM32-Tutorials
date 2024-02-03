4d25d46dc9508d4b49ce4bcc9b2f10afdf3b0013
bevybizzle <bevybizzle@googlemail.com>
2024-02-03 14:51:33 +0000

    added ADC files

--- ADC Polling/Core/Src/main.c
+++ ADC Polling/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -32,6 +33,10 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+#define ADC_RESOLUTION 4096u
+#define REF_VOLTAGE 3.3f
+#define VOLT_PER_BIT (REF_VOLTAGE / (float)ADC_RESOLUTION)
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,13 +45,11 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
-// Static constant pointer to the hello world buffer we are transmitting.
-static const char *hello_world_buffer = "Hello Jupiter!\r\n";
-static HAL_StatusTypeDef ok_notok_s = HAL_OK;
 
 /* USER CODE END PV */
 
@@ -54,6 +57,7 @@ static HAL_StatusTypeDef ok_notok_s = HAL_OK;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -92,7 +96,16 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
+
+  (void)HAL_ADC_Start(&hadc1);  // We must start the ADC
+  uint32_t adc_val_read = 0u; // value read from ADC
+  float calculated_voltage = 0.0f; // output voltage calculated
+
+  char buffer[100] = "";
+  HAL_StatusTypeDef ok_notok = HAL_BUSY;
+
 
   /* USER CODE END 2 */
 
@@ -103,21 +116,31 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  // First we transmit the hello world buffer out the transmit pin of UART2.
-	  ok_notok_s = HAL_UART_Transmit(&huart2, (uint8_t *)hello_world_buffer, 17, 100);
 
-	  if(ok_notok_s == HAL_OK)
+	  ok_notok = HAL_ADC_PollForConversion(&hadc1, 100);
+
+	  if (ok_notok == HAL_OK)
 	  {
-		  // If the UART transmission is okay then the green LED will stay off.
+		  // First we need to read the ADC value
+		  adc_val_read = HAL_ADC_GetValue(&hadc1);
+
+		  // Then we can convert it based on the calculation of the volt per bit value.
+		  calculated_voltage = VOLT_PER_BIT * adc_val_read;
+
+		  // Then we can add this to a buffer to be sent out over the UART.
+		  snprintf(buffer, 100, "Voltage read from ADC %.6f\r\n", calculated_voltage);
+
+		  // Finally we can send the data out over the UART.
+		  ok_notok = HAL_UART_Transmit(&huart2, (uint8_t *)buffer, 100, 100);
 		  HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
 	  }
 	  else
 	  {
-		  // If the UART transmission is not okay then the green LED will be turned on.
 		  HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_SET);
 	  }
-	  // Finally there is a delay of 1 Second.
+
 	  HAL_Delay(1000);
+
   }
   /* USER CODE END 3 */
 }
@@ -130,6 +153,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -158,6 +182,78 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC12;
+  PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_MultiModeTypeDef multimode = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the ADC multi-mode
+  */
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
